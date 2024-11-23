f409eff0a6824ed28e29c7ed03979b7185e3ee5b
tanish-garg-1 <130556147+tanish-garg-1@users.noreply.github.com>
2024-11-23 12:18:54 +0530

    Time: 1 ms (99.48%), Space: 44.7 MB (74.17%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        int count=0;
         int i=0,j=0;
         sort(g.begin(),g.end());
         sort(s.begin(),s.end());
         while(i!=g.size() && j!=s.size()){
-            if(g[i]>s[j]) j++;
-            else{
-                count++;i++;j++;
-            }
+            if(g[i]<=s[j]) i++;
+            j++;
         }
-        return count;
+        return i;
     }
 };
