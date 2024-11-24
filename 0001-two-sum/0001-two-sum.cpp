4b2ab1217c9175197e5af66767ab8541eda1ed1a
tanish-garg-1 <130556147+tanish-garg-1@users.noreply.github.com>
2024-11-24 18:38:01 +0530

    Time: 0 ms (100%), Space: 13.9 MB (50.68%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,6 +1,30 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        vector<pair<int,int>> arr;
+        for(int i=0;i<nums.size();i++){
+            arr.push_back({nums[i],i});
+        }
+        sort(arr.begin(),arr.end(),greater<>());
+        int i=0,j=nums.size()-1;
+        vector<int>ans;
+        while(i<j){
+            if(arr[i].first+arr[j].first==target){
+                ans.push_back(arr[i].second);ans.push_back(arr[j].second);
+                break;
+            }
+            else if(arr[i].first+arr[j].first<target) j--;
+            else i++;
+        }
+        return ans;
+    }
+};
+
+
+/*
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int> my_map;
         for(int i=0;i<nums.size();i++){
             my_map[nums[i]]=i;
@@ -16,3 +40,4 @@ public:
         return ans;
     }
 };
+*/
