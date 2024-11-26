90379be8ec3a5fbd7ef1b926d16a38e30222d958
tanish-garg-1 <130556147+tanish-garg-1@users.noreply.github.com>
2024-11-26 15:54:53 +0530

    Time: 13 ms (29.71%), Space: 7.6 MB (11.22%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,11 +1,7 @@
-class Solution {
-    int function(int n){
-        if(n<=1) return n;
-        return function(n-1)+function(n-2);
-    }
+class Solution { 
 public:
     int fib(int n) {
-        int ans=function(n);
-        return ans;
+        if(n<=1) return n;
+        return fib(n-1)+fib(n-2);
     }
 };
