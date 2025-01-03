bfd3f791bcc7a16b816936fd264b3fd87687f69e
tanish-garg-1 <130556147+tanish-garg-1@users.noreply.github.com>
2025-01-03 11:49:43 +0530

    Time: 66 ms (24.61%), Space: 94.4 MB (12.17%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -5,7 +5,7 @@ public:
             if(nums[i]%2==0) nums[i]=0;
             else nums[i]=1;
         }
-        map<int,int> mymap;
+        unordered_map<int,int> mymap;
         mymap[0]++;
         int ans=0;
         for(int i=0;i<nums.size();i++){
