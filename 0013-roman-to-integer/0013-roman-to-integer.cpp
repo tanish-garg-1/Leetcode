719eebb9dfa69b91778a53ff34a9c78cfd82f50d
tanish-garg-1 <130556147+tanish-garg-1@users.noreply.github.com>
2025-02-04 18:20:10 +0530

    Time: 70 ms (5.59%), Space: 12.4 MB (58.3%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,13 +1,4 @@
 class Solution {
-    int find_val(char x){
-          if(x=='I')  return 1;
-            else if(x=='V') return 5;
-            else if(x=='X') return 10;
-            else if(x=='L') return 50;
-            else if(x=='C') return 100;
-            else if(x=='D') return 500;
-            else return 1000;
-    }
 public:
     int romanToInt(string s) {
         unordered_map<char,int>mymap;
