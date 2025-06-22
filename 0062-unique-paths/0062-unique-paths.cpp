25ffe249558d89cb35dfbdcddd969bad23c9d667
tanish-garg-1 <130556147+tanish-garg-1@users.noreply.github.com>
2025-06-22 23:39:54 +0530

    Time: 0 ms (100%), Space: 9.4 MB (33.42%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,22 +1,13 @@
-#include <vector>
-using namespace std;
-
 class Solution {
-    int function(int m, int n, int i, int j, vector<vector<int>>& arr) {
-
-        if (i == m - 1 && j == n - 1) return 1;
-
-        if (i >= m || j >= n) return 0;
-
-        if (arr[i][j] != -1) return arr[i][j];
-
-        arr[i][j] = function(m, n, i + 1, j, arr) + function(m, n, i, j + 1, arr);
-        return arr[i][j];
+    int recursion(int i,int j,vector<vector<int>>& dp){
+        if(i==0 && j==0) return 1;
+        if(i<0 || j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        return dp[i][j]=recursion(i-1,j,dp)+recursion(i,j-1,dp);
     }
-
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> arr(m, vector<int>(n, -1));
-        return function(m, n, 0, 0, arr);
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return recursion(m-1,n-1,dp);
     }
 };
