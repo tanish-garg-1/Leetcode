3b2aab95d3e77a951cb0177e6d882fd36bbc848d
tanish-garg-1 <130556147+tanish-garg-1@users.noreply.github.com>
2025-07-02 09:55:08 +0530

    Time: 164 ms (34.77%), Space: 64.9 MB (7.23%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,16 +1,41 @@
+// class Solution {
+//     int recursion(int i,int target,vector<int>& nums,vector<vector<int>>& dp){
+//         if(target==0 && i<0) return 1;
+//         if(i<0) return 0;
+//         if(dp[i][target+10000] != -1) return dp[i][target+10000];
+//         int left=recursion(i-1,target-nums[i],nums,dp);
+//         int right=recursion(i-1,target+nums[i],nums,dp);
+//         return dp[i][target+10000]=left+right;
+//     }
+// public:
+//     int findTargetSumWays(vector<int>& nums, int target) {
+//         int n=nums.size();
+//         vector<vector<int>> dp(n,vector<int>(n*1000 +1,-1));
+//         return recursion(n-1,target,nums,dp);
+//     }
+// };
+
 class Solution {
-    int recursion(int i,int target,vector<int>& nums,vector<vector<int>>& dp){
+    struct pair_hash {
+        size_t operator()(const pair<int, int>& p) const {
+            return hash<int>()(p.first) ^ hash<int>()(p.second << 1);
+        }
+    };
+
+    int recursion(int i,int target,vector<int>& nums,unordered_map<pair<int,int>,int,pair_hash>& mymap){
         if(target==0 && i<0) return 1;
         if(i<0) return 0;
-        if(dp[i][target+10000] != -1) return dp[i][target+10000];
-        int left=recursion(i-1,target-nums[i],nums,dp);
-        int right=recursion(i-1,target+nums[i],nums,dp);
-        return dp[i][target+10000]=left+right;
+        pair<int, int> key={i,target};
+        if(mymap.find(key) != mymap.end()) return mymap[key];
+        // if(dp[i][target+10000] != -1) return dp[i][target+10000];
+        int left=recursion(i-1,target-nums[i],nums,mymap);
+        int right=recursion(i-1,target+nums[i],nums,mymap);
+        return mymap[key]=left+right;
     }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
-        vector<vector<int>> dp(n,vector<int>(20001,-1));
-        return recursion(n-1,target,nums,dp);
+        unordered_map<pair<int,int>,int,pair_hash> mymap;
+        return recursion(n-1,target,nums,mymap);
     }
 };
