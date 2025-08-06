55fb7a56fa8cfba9977dce32dad1f52662aefca2
tanish-garg-1 <130556147+tanish-garg-1@users.noreply.github.com>
2025-08-06 18:20:38 +0530

    Time: 8 ms (58.98%), Space: 71.5 MB (44.95%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -1,20 +1,19 @@
 class Solution {
+    int find(vector<int>& nums, int k){
+        int i=0,j=0,ans=0;
+        while(j<nums.size()){
+            if(nums[j]%2==1) k--;
+            j++;
+            while(k<0){
+                if(nums[i]%2==1) k++;
+                i++;
+            }
+            ans += (j - i);
+        }
+        return ans;       
+    }
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]%2==0) nums[i]=0;
-            else nums[i]=1;
-        }
-        unordered_map<int,int> mymap;
-        mymap[0]++;
-        int ans=0;
-        for(int i=0;i<nums.size();i++){
-            if(i!=0) nums[i]=nums[i]+nums[i-1];
-            if(mymap.find(nums[i]-k)!=mymap.end()){
-                ans=ans+mymap[nums[i]-k];
-            }
-            mymap[nums[i]]++;
-        }
-        return ans;
+        return (find(nums,k)-find(nums,k-1));
     }
 };
