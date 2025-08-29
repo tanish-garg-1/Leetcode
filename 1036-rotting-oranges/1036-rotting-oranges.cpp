d692829fca0659ad15185086d65245bb75736705
tanish-garg-1 <130556147+tanish-garg-1@users.noreply.github.com>
2025-08-29 17:09:12 +0530

    Time: 0 ms (100%), Space: 16.8 MB (60.66%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,51 +1,43 @@
 class Solution {
+    void rott_oranges(queue<pair<int,int>> &q,int row,int col,vector<vector<int>>& grid,int &fresh){
+        if(row>0 && grid[row-1][col]==1) {
+            grid[row-1][col]=2;q.push({row-1,col});fresh--;
+        }
+        if(col>0 && grid[row][col-1]==1) {
+            grid[row][col-1]=2;q.push({row,col-1});fresh--;
+        }
+        if(row<grid.size()-1 && grid[row+1][col]==1) {
+            grid[row+1][col]=2;q.push({row+1,col});fresh--;
+        }
+        if(col<grid[0].size()-1 && grid[row][col+1]==1) {
+            grid[row][col+1]=2;q.push({row,col+1});fresh--;
+        }
+        return;
+    }
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        unordered_map<int,int> fresh;
-        unordered_map<int,int> rotten;
-        int rows=grid.size();
-        int cols=grid[0].size();
-        const int sizee=rows*cols;
-        vector<vector<int>> adj(sizee);
-
-
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                if(grid[i][j]==1) fresh[i*cols+j]++;
-                else if(grid[i][j]==2) rotten[i*cols+j]++;
-                if(grid[i][j]!=0){
-                    if(i>0 && grid[i-1][j]!=0) adj[i*cols+j].push_back((i-1)*cols+j);
-                    if(i<rows-1 && grid[i+1][j]!=0) adj[i*cols+j].push_back((i+1)*cols+j);
-                    if(j>0 && grid[i][j-1]!=0) adj[i*cols+j].push_back(i*cols+(j-1));
-                    if(j<cols-1 && grid[i][j+1]!=0) adj[i*cols+j].push_back(i*cols+(j+1));
+        queue<pair<int,int>> q;
+        int rotten=0,fresh=0,empty=0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]==0) empty++;
+                if(grid[i][j]==1) fresh++;
+                if(grid[i][j]==2){ 
+                    q.push({i,j});rotten++;
                 }
             }
         }
-
-        vector<bool> visited(sizee,false);
-        queue<int> q;
-        int ans=0;
-        int prev_trev=0,total_trev=0;
-        for(auto it=rotten.begin();it!=rotten.end();it++) {
-            q.push(it->first);total_trev++;visited[it->first]=true;
-        }
-        if(q.empty() && fresh.size()==0) return 0;
-        cout<<total_trev<<endl;
+        int ans=-1;
+        if(rotten==0) ans=0;
         while(!q.empty()){
-            int q_size=q.size();
-            if(total_trev==rotten.size()+fresh.size()) return ans;
-            while(q_size--){
-                int temp=q.front();
+            int n=q.size();
+            while(n--){
+                rott_oranges(q,q.front().first,q.front().second,grid,fresh);
                 q.pop();
-                for(auto it:adj[temp]){
-                    if(visited[it]==false){
-                        visited[it]=true;q.push(it);total_trev++;
-                    }
-                }
             }
-            cout<<total_trev<<" "<<ans<<endl;
             ans++;
         }
-        return -1;
+        if(fresh==0) return ans;
+        else return -1;
     }
 };
