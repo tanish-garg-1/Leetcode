2badd0e29577ce06aace8c92019ebec6cbdc47d0
tanish-garg-1 <130556147+tanish-garg-1@users.noreply.github.com>
2026-05-20 21:49:46 +0530

    Time: 2 ms (50.08%), Space: 39.6 MB (28.01%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    bool jump(int i,vector<int>& arr, vector<bool> visited){
+    bool jump(int i,vector<int>& arr, vector<bool>& visited){
         if(i<0 || i>=arr.size() || visited[i]==true) return false;
         visited[i]=true;
         if(arr[i]==0) return true;
